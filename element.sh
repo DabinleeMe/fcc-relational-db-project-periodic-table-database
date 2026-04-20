0dc94d40b7902a42c6300a7fa7deb7a001fc7002
DabinleeMe <124752900+DabinleeMe@users.noreply.github.com>
2026-04-20 10:21:04 -0400

    feat: add Query logic based on input

--- element.sh
+++ element.sh
@@ -12,3 +12,5 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 # 3. Query logic based on input ($1)
 # (Need to determine if $1 is an atomic_number, symbol, or name)
+ELEMENT_DATA=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE $WHERE_CONDITION")
+
